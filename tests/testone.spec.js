62d9a0919d67fcf47e870ca53b283ddaddcbd607
Michael Shea <m.shea2481@gmail.com>
2026-06-12 22:43:16 -0400

    add pages and update styles

---
+++ tests/testone.spec.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+// Click the get started link.
+  await page.getByRole('link', { name: 'LinkedIn' }).click();
+
+// Expects page to have a heading with the name of Installation.
+  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Michael Shea | LinkedIn/);
+});
+
